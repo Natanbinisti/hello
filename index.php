436f14ac487ea727160ba7105cc0306a071a37e9
natanbinisti <natanbinistilyon@gmail.com>
2023-11-20 16:58:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$prenom = "je sais pas comment tu t'appelles";
+$secret = "c'etais pas une final de fou";
+$userAutorise = "Faker";
+$motDePasse = "azirmid";
+$contenu = "entre le bon username et mot de passe pour que te sois révélé le secret";
+
+//si bon username mais mauvais mot de passe, afficher "mauvais mot de passe"
+//si mauvais username, afficher "déso je te connais pas toi la"
+//si bon username et bon mot de passe, afficher le secret
+
+if(isset($_GET['prenom'])){
+    $prenom = $_GET['prenom'];
+}
+if (isset($_GET['username'])) {
+    $userAutorise = $_GET ['prenom'] ;
+    echo $secret ;
+
+}
+
+?>
+
+<h2>Hello <?= $prenom ?> </h2>
+
+<form method="get" action="">
+
+    <input type="text" name="prenom" id="">
+    <button type="submit">OK</button>
+
+</form>
+
+<hr>
+<hr>
+<form action="">
+    <input placeholder="username" type="text" name="username" id="">
+    <input placeholder="password" type="password" name="password" id="">
+    <button type="submit">OK</button>
+</form>
+<p><?= $contenu ?></p>
+</body>
+</html>
